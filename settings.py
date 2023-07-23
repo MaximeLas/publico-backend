ad6d20c100743b47cd25916b8b6c01f3f10f9ac5
Maxime Las <Publico@users.noreply.huggingface.co>
2023-07-23 14:39:43 +0000

    Install version gradio 3.35.0 to support ClearButton

--- settings.py
+++ settings.py
@@ -15,6 +15,12 @@ if sqlite3.sqlite_version_info < (3, 35, 0):
 
     print(f'\ninstalled sqlite3 version={sys.modules["sqlite3"].sqlite_version_info}\n')
 
+import importlib.metadata
+if importlib.metadata.version('gradio') < '3.35.0':
+    print(f'gradio version is too old to use ClearButton version={importlib.metadata.version("gradio")}')
+    # installing gradio version = 3.35.0 in order to use ClearButton
+    subprocess.check_call([sys.executable, "-m", "pip", "install", "gradio==3.35.0"])
+    print(f'installed gradio version={importlib.metadata.version("gradio")}\n')
 
 # get arguments from command line and set GPT_MODEL
 GPT_MODEL = 'gpt-3.5-turbo' if len(sys.argv) > 1 else 'gpt-4'
