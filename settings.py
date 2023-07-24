9b751ee2a7d403c432ead65086fd54faf100deaa
Maxime Las <maximelas1@gmail.com>
2023-07-24 03:47:55 +0100

    Clean settings.py

--- settings.py
+++ settings.py
@@ -19,14 +19,6 @@ if sqlite3.sqlite_version_info < (3, 35, 0):
 
     print(f'\ninstalled sqlite3 version={sys.modules["sqlite3"].sqlite_version_info}\n')
 
-# remove below once hf version works again
-import importlib.metadata
-print(f'current gradio version={importlib.metadata.version("gradio")}\n')
-'''if importlib.metadata.version('gradio') < '3.35.0':
-    print(f'gradio version is too old to use ClearButton version={importlib.metadata.version("gradio")}')
-    # installing gradio version = 3.35.0 in order to use ClearButton
-    subprocess.check_call([sys.executable, "-m", "pip", "install", "gradio==3.35.0"])
-    print(f'installed gradio version={importlib.metadata.version("gradio")}\n')'''
 
 # get arguments from command line and set GPT_MODEL
 GPT_MODEL = 'gpt-3.5-turbo' if len(sys.argv) > 1 else 'gpt-4'
